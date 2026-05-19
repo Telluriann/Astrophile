0868ef25383d16046f1e71b58731aeca1e47e664
Pranav <pranav.tellurian@gmail.com>
2026-05-19 08:49:45 +0200

    renamed cosmopedia to astrophile

--- astrophile/urls.py
+++ astrophile/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cosmopedia project.
+URL configuration for astrophile project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
--- astrophile/wsgi.py
+++ astrophile/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cosmopedia project.
+WSGI config for astrophile project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cosmopedia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astrophile.settings')
 
 application = get_wsgi_application()
